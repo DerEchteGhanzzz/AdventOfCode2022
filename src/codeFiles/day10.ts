08b4b4fb90587caad5eecb799e39ccb12611431e
Ghanzzz <blankensteijnhans@gmail.com>
2022-12-10 22:29:51 +0100

    until day 11

---
+++ src/codeFiles/day10.ts
@@ -0,0 +1,31 @@
+export function solveA(lines: string){
+    let input: string[] = parseLines(lines);
+    let answer: string
+
+    for (let entry of input){
+
+    }
+
+    return answer
+}
+
+export function solveB(lines: string){
+    let input: string[] = parseLines(lines);
+    let answer: string
+
+    for (let entry of input){
+
+    }
+
+    return answer
+}
+
+function parseLines(inputString: string): string[]{
+    let lines = inputString.split(/\r?\n/);
+
+    for (let line of lines){
+
+    }
+
+    return lines;
+}
