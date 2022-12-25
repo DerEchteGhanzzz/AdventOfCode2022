11f81ea57f118655c4e9500fb90a1946b5af8ad4
DerEchteGhanzzz <79211646+DerEchteGhanzzz@users.noreply.github.com>
2022-12-25 14:01:01 +0100

    Update day17

--- src/codeFiles/day17.js
+++ src/codeFiles/day17.js
@@ -1,181 +1,168 @@
-"use strict";
-var __read = (this && this.__read) || function (o, n) {
-    var m = typeof Symbol === "function" && o[Symbol.iterator];
-    if (!m) return o;
-    var i = m.call(o), r, ar = [], e;
-    try {
-        while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
-    }
-    catch (error) { e = { error: error }; }
-    finally {
-        try {
-            if (r && !r.done && (m = i["return"])) m.call(i);
-        }
-        finally { if (e) throw e.error; }
-    }
-    return ar;
-};
-var __values = (this && this.__values) || function(o) {
-    var s = typeof Symbol === "function" && Symbol.iterator, m = s && o[s], i = 0;
-    if (m) return m.call(o);
-    if (o && typeof o.length === "number") return {
-        next: function () {
-            if (o && i >= o.length) o = void 0;
-            return { value: o && o[i++], done: !o };
-        }
-    };
-    throw new TypeError(s ? "Object is not iterable." : "Symbol.iterator is not defined.");
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.solveB = exports.solveA = void 0;
-function solveA(lines) {
-    var _a;
-    var currents = parseLines(lines);
-    var coordSet = new Set();
-    var highestY = 0;
-    var blocks = [
-        new TetrisBlock([[0, 0], [1, 0], [2, 0], [3, 0]], 0, 3),
-        new TetrisBlock([[1, 0], [0, 1], [1, 1], [2, 1], [1, 2]], 2, 2),
-        new TetrisBlock([[0, 0], [1, 0], [2, 0], [2, 1], [2, 2]], 2, 2),
-        new TetrisBlock([[0, 0], [0, 1], [0, 2], [0, 3]], 3, 0),
-        new TetrisBlock([[0, 0], [1, 0], [0, 1], [1, 1]], 1, 1)
-    ];
-    var currentIndex = 0;
-    for (var i = 0; i < 2022; i++) {
-        var currentBlock = blocks[i % 5];
-        var newY = void 0;
-        var _ = void 0;
-        _a = __read(simulateTetris(coordSet, currentBlock, currents, highestY + 3, currentIndex), 3), newY = _a[0], currentIndex = _a[1], _ = _a[2];
+export function solveA(lines: string) {
+    let currents: number[] = parseLines(lines);
+    let coordSet = new Set<string>();
+    let highestY = 0;
+
+    const blocks = [
+        new TetrisBlock([[0, 0], [1, 0], [2, 0], [3, 0]], 0, 3, "-"),
+        new TetrisBlock([[1, 0], [0, 1], [1, 1], [2, 1], [1, 2]], 2, 2, "+"),
+        new TetrisBlock([[0, 0], [1, 0], [2, 0], [2, 1], [2, 2]], 2, 2, "L"),
+        new TetrisBlock([[0, 0], [0, 1], [0, 2], [0, 3]], 3, 0, "|"),
+        new TetrisBlock([[0, 0], [1, 0], [0, 1], [1, 1]], 1, 1, "■")
+    ]
+    let currentIndex = 0;
+    for (let i = 0; i < 2022; i++) {
+
+        const currentBlock = blocks[i % 5];
+        //console.log(coordSet);
+        let newY;
+        let _;
+        [newY, currentIndex, _] = simulateTetris(coordSet, currentBlock, currents, highestY + 3, currentIndex, i);
         if (newY > highestY) {
             highestY = newY;
         }
+        //console.log()
     }
+
     return highestY;
 }
-exports.solveA = solveA;
-function solveB(lines) {
-    var _a;
-    var currents = parseLines(lines);
-    var coordSet = new Set();
-    var blocks = [
-        new TetrisBlock([[0, 0], [1, 0], [2, 0], [3, 0]], 0, 3),
-        new TetrisBlock([[1, 0], [0, 1], [1, 1], [2, 1], [1, 2]], 2, 2),
-        new TetrisBlock([[0, 0], [1, 0], [2, 0], [2, 1], [2, 2]], 2, 2),
-        new TetrisBlock([[0, 0], [0, 1], [0, 2], [0, 3]], 3, 0),
-        new TetrisBlock([[0, 0], [1, 0], [0, 1], [1, 1]], 1, 1)
-    ];
-    var currentIndex = 0;
-    var xyDict = new Map();
-    var highestY = 0;
-    for (var i = 0; i < 1000000000000; i++) {
-        var currentBlock = blocks[i % 5];
+
+export function solveB(lines: string) {
+    let currents: number[] = parseLines(lines);
+    let coordSet = new Set<string>();
+
+    let currentIndex = 0;
+    let xyDict = new Map();
+
+    const blocks = [
+        new TetrisBlock([[0, 0], [1, 0], [2, 0], [3, 0]], 0, 3, "-"),
+        new TetrisBlock([[1, 0], [0, 1], [1, 1], [2, 1], [1, 2]], 2, 2, "+"),
+        new TetrisBlock([[0, 0], [1, 0], [2, 0], [2, 1], [2, 2]], 2, 2, "L"),
+        new TetrisBlock([[0, 0], [0, 1], [0, 2], [0, 3]], 3, 0, "|"),
+        new TetrisBlock([[0, 0], [1, 0], [0, 1], [1, 1]], 1, 1, "■")
+    ]
+    const iterations = 1000000000000;
+    let highestY = 0;
+    const solutionMap = {blockDiff: 0, heightDiff: 0, startBlocks: 0, startHeight: 0};
+    for (let i = 0; i < iterations; i++) {
+        const currentBlock = blocks[i % 5];
+
+        let newY;
+        let _;
+        [newY, currentIndex, _] = advancedTetris(coordSet, currentBlock, currents, highestY + 3, currentIndex, i, solutionMap);
+        if (_) {
+            break;
+        }
+        if (newY > highestY) {
+            highestY = newY;
+        }
         xyDict.set(i, highestY);
-        if (xyDict.get(i) * Math.floor(1000000000000 / i) + xyDict.get(1000000000000 % i) === 1514285714288) {
-            return i + " " + currentIndex;
-        }
-        var newY = void 0;
-        var landedLeftX = void 0;
-        _a = __read(simulateTetris(coordSet, currentBlock, currents, highestY + 3, currentIndex), 3), newY = _a[0], currentIndex = _a[1], landedLeftX = _a[2];
-        if (newY > highestY) {
-            highestY = newY;
-        }
     }
-    return highestY;
+    
+    return Math.floor(iterations / solutionMap.blockDiff) * solutionMap.heightDiff + xyDict.get(iterations%solutionMap.blockDiff) - 1
 }
-exports.solveB = solveB;
-function simulateTetris(coordSet, currentBlock, currents, startingY, currentIndex) {
-    var _a, e_1, _b;
-    var down = 0;
-    var xLeft = 2;
-    var transposedCoords;
+
+
+function advancedTetris(coordSet: Set<string>, currentBlock: TetrisBlock, currents: number[], startingY: number, currentIndex: number, i,
+                        solutionMap: {blockDiff: number, heightDiff: number, startBlocks: number, startHeight: number}): [number, number, boolean] {
+
+    let down = 0;
+    let xLeft = 2;
+    let transposedCoords;
     while (true) {
-        var currentCurrent = currents[currentIndex % currents.length];
-        currentIndex++;
-        _a = __read(currentBlock.transposedCoordset(xLeft, startingY - down, coordSet, currentCurrent), 2), transposedCoords = _a[0], xLeft = _a[1];
-        try {
-            for (var transposedCoords_1 = (e_1 = void 0, __values(transposedCoords)), transposedCoords_1_1 = transposedCoords_1.next(); !transposedCoords_1_1.done; transposedCoords_1_1 = transposedCoords_1.next()) {
-                var transposedSegment = transposedCoords_1_1.value;
-                if (coordSet.has(JSON.stringify([transposedSegment[0], transposedSegment[1] - 1])) || transposedSegment[1] - 1 < 0) {
-                    transposedCoords.forEach(function (a) { return coordSet.add(JSON.stringify(a)); });
-                    return [currentBlock.highestY + 1 + startingY - down, currentIndex, transposedSegment[0]];
-                }
+        let currentCurrent = currents[currentIndex % currents.length];
+        if (currentIndex % currents.length === 0 && startingY % 15 === 0 && currentBlock.shape === "|") {
+            if (solutionMap.startBlocks === 0) {
+                solutionMap.startBlocks = i;
+                solutionMap.startHeight = startingY - 3;
+            } else {
+                solutionMap.blockDiff = i - solutionMap.startBlocks;
+                solutionMap.heightDiff = startingY - 3 - solutionMap.startHeight;
+                return [currentBlock.highestY + 1 + startingY - down, currentIndex, true]
             }
+            console.log(currentBlock.shape, startingY-3,i);
         }
-        catch (e_1_1) { e_1 = { error: e_1_1 }; }
-        finally {
-            try {
-                if (transposedCoords_1_1 && !transposedCoords_1_1.done && (_b = transposedCoords_1.return)) _b.call(transposedCoords_1);
+        currentIndex++;
+
+        [transposedCoords, xLeft] = currentBlock.transposedCoordset(
+            xLeft, startingY - down,
+            coordSet, currentCurrent)
+        //console.log(transposedCoords[2], currentCurrent)
+
+        for (let transposedSegment of transposedCoords) {
+
+            if (coordSet.has(JSON.stringify([transposedSegment[0], transposedSegment[1] - 1])) || transposedSegment[1] - 1 < 0) {
+                transposedCoords.forEach((a) => coordSet.add(JSON.stringify(a)));
+                return [currentBlock.highestY + 1 + startingY - down, currentIndex, false];
             }
-            finally { if (e_1) throw e_1.error; }
         }
         down++;
     }
+
 }
-function parseLines(inputString) {
-    var e_2, _a;
-    var lines = inputString.split(/\r?\n/);
-    var currents = [];
-    try {
-        for (var _b = __values(lines[0].split("")), _c = _b.next(); !_c.done; _c = _b.next()) {
-            var char = _c.value;
-            if (char === '<') {
-                currents.push(-1);
-            }
-            else {
-                currents.push(1);
+
+function simulateTetris(coordSet: Set<string>, currentBlock: TetrisBlock, currents: number[], startingY: number, currentIndex: number, i): [number, number, number] {
+
+    let down = 0;
+    let xLeft = 2;
+    let transposedCoords;
+    while (true) {
+        let currentCurrent = currents[currentIndex % currents.length];
+        if (currentIndex % currents.length === 0 && startingY % 15 === 0 && currentBlock.shape === "|") {
+            console.log(currentBlock.shape, startingY-3,i);
+        }
+        currentIndex++;
+
+        [transposedCoords, xLeft] = currentBlock.transposedCoordset(
+            xLeft, startingY - down,
+            coordSet, currentCurrent)
+        //console.log(transposedCoords[2], currentCurrent)
+
+        for (let transposedSegment of transposedCoords) {
+
+            if (coordSet.has(JSON.stringify([transposedSegment[0], transposedSegment[1] - 1])) || transposedSegment[1] - 1 < 0) {
+                transposedCoords.forEach((a) => coordSet.add(JSON.stringify(a)));
+                return [currentBlock.highestY + 1 + startingY - down, currentIndex, transposedSegment[0]];
             }
         }
+        down++;
     }
-    catch (e_2_1) { e_2 = { error: e_2_1 }; }
-    finally {
-        try {
-            if (_c && !_c.done && (_a = _b.return)) _a.call(_b);
+
+}
+
+function parseLines(inputString: string): number[] {
+    const lines = inputString.split(/\r?\n/);
+    const currents = [];
+    for (let char of lines[0].split("")) {
+        if (char === '<') {
+            currents.push(-1);
+        } else {
+            currents.push(1);
         }
-        finally { if (e_2) throw e_2.error; }
     }
     return currents;
 }
-var TetrisBlock = (function () {
-    function TetrisBlock(coordSet, highestY, right) {
-        this.coordSet = coordSet;
-        this.highestY = highestY;
-        this.right = right;
+
+class TetrisBlock {
+    constructor(public coordSet: number[][], public highestY: number, public right: number, public shape: string) {
     }
-    TetrisBlock.prototype.transposedCoordset = function (xLeft, dy, coordSet, current) {
-        var e_3, _a, e_4, _b;
-        var transposedCoords = [];
-        var dx = xLeft + current;
-        try {
-            for (var _c = __values(this.coordSet), _d = _c.next(); !_d.done; _d = _c.next()) {
-                var coord = _d.value;
-                if (coord[0] + dx < 0 || coord[0] + dx === 7 ||
-                    coordSet.has(JSON.stringify([coord[0] + dx, coord[1] + dy]))) {
-                    dx -= current;
-                }
+
+    public transposedCoordset(xLeft, dy, coordSet: Set<string>, current: number): [number[][], number] {
+        const transposedCoords: number[][] = [];
+
+        let dx = xLeft + current;
+
+        for (let coord of this.coordSet) {
+
+            if (coord[0] + dx < 0 || coord[0] + dx === 7 ||
+                coordSet.has(JSON.stringify([coord[0] + dx, coord[1] + dy]))) {
+                dx -= current;
             }
         }
-        catch (e_3_1) { e_3 = { error: e_3_1 }; }
-        finally {
-            try {
-                if (_d && !_d.done && (_a = _c.return)) _a.call(_c);
-            }
-            finally { if (e_3) throw e_3.error; }
+        for (let coord of this.coordSet) {
+            transposedCoords.push([coord[0] + dx, coord[1] + dy]);
         }
-        try {
-            for (var _e = __values(this.coordSet), _f = _e.next(); !_f.done; _f = _e.next()) {
-                var coord = _f.value;
-                transposedCoords.push([coord[0] + dx, coord[1] + dy]);
-            }
-        }
-        catch (e_4_1) { e_4 = { error: e_4_1 }; }
-        finally {
-            try {
-                if (_f && !_f.done && (_b = _e.return)) _b.call(_e);
-            }
-            finally { if (e_4) throw e_4.error; }
-        }
+
         return [transposedCoords, dx];
-    };
-    return TetrisBlock;
-}());
-//# sourceMappingURL=day17.js.map
+    }
+}
